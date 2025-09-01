4a6045ed06dffab6c347fddf390ca8e92e5ff88e
Ankit Sinha <ankits.brainerhub@gmail.com>
2025-09-01 15:31:44 +0530

    added the changes

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
-  plugins: [react(),  tailwindcss(), ],
+  plugins: [react(), tailwindcss(), ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
