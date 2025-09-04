0928b67a338a30b5b20016e0b07b8cc38a1eedf5
Ankit Sinha <ankits.brainerhub@gmail.com>
2025-09-04 16:48:03 +0530

    Added config file

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), ],
+  server: {
+    historyApiFallback: true
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
